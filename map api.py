99e8acc81d00138d312b71565d8d34e185bb2261
chrissbsh <62696389+chrissbsh@users.noreply.github.com>
2020-05-20 16:31:01 +0200

    Update map api.py

--- map api.py
+++ map api.py
@@ -6,51 +6,76 @@ import googlemaps
 
 gmaps = googlemaps.Client(key='KEY')
 
-rue = input("adresse : ")
-codepostal = input("code postal : ")
-ville = input("ville : ")
+L=input("autoriser la localisation , oui ou non ? :")
 
-R0=rue.replace("â", "a").replace("È", "e").replace("É", "e").replace("è", "e").replace("é", "e").replace(" ", "+").replace("'", "%27")
+if "OUI"in L.upper():
+    url1 = ("http://api.ipstack.com/check?access_key='key")
+    q = urllib.request.urlopen(url1)
+    data = json.load(q)
+    # print(data)
 
-V0=ville.replace("â", "a").replace("È", "e").replace("É", "e").replace("è", "e").replace("é", "e").replace(" ", "+").replace("'", "%27")
+    ip = data['ip']
+    # print(ip)
 
-R1=R0.upper()
-CP1=codepostal.upper()
-V1=V0.upper()
+    url = ("http://api.ipstack.com/" + ip + "?access_key='key")
 
-Q1=(R1+","+CP1+"+"+V1+",")
-print(Q1)
+    q = urllib.request.urlopen(url)
+    data = json.load(q)
+    print(data)
 
-url =("https://maps.googleapis.com/maps/api/geocode/json?address="+Q1+"+FRANCE&key='KEY'")
+    l2 = (data['latitude'])
+    ln2 = (data['longitude'])
 
-print(url)
+    print(l2)
+    print(ln2)
 
-#geocoding
-q = urllib.request.urlopen(url)
+else:
 
-data = json.load(q)
-#print(data)
+    rue = input("adresse : ")
+    codepostal = input("code postal : ")
+    ville = input("ville : ")
 
-geocode_result = gmaps.geocode(Q1)
-#print(geocode_result)
+    R0=rue.replace("â", "a").replace("È", "e").replace("É", "e").replace("è", "e").replace("é", "e").replace(" ", "+").replace("'", "%27")
 
-print(geocode_result[0]['geometry']['location']['lat'])
-print(geocode_result[0]['geometry']['location']['lng'])
+    V0=ville.replace("â", "a").replace("È", "e").replace("É", "e").replace("è", "e").replace("é", "e").replace(" ", "+").replace("'", "%27")
 
-lat=geocode_result[0]['geometry']['location']['lat']
-lng=geocode_result[0]['geometry']['location']['lng']
+    R1=R0.upper()
+    CP1=codepostal.upper()
+    V1=V0.upper()
 
-l2 = float(lat)
-ln2 = float(lng)
+    Q1=(R1+"%2C"+CP1+"+"+V1+"%2C")
+    print(Q1)
 
-l1=float(48.6333)
-ln1=float(2.45)
+    url =("https://maps.googleapis.com/maps/api/geocode/json?address="+Q1+"+FRANCE&autocomplete=1&key='key")
+
+    print(url)
+
+    #geocoding
+    q = urllib.request.urlopen(url)
+
+    data = json.load(q)
+    #print(data)
+
+    geocode_result = gmaps.geocode(Q1)
+    #print(geocode_result)
+
+    print(geocode_result[0]['geometry']['location']['lat'])
+    print(geocode_result[0]['geometry']['location']['lng'])
+
+    lat=geocode_result[0]['geometry']['location']['lat']
+    lng=geocode_result[0]['geometry']['location']['lng']
+
+    l2 = float(lat)
+    ln2 = float(lng)
+
+l1=float(48.637976)
+ln1=float(2.443590)
 
 A=fabs(l1-l2)
-print("A:" , A)
+#print("A:" , A)
 
 B=fabs(ln1-ln2)
-print("B:" , B)
+#print("B:" , B)
 
 #angle
 alpha=float(tan(B/A))
@@ -60,7 +85,7 @@ ALPHA=degrees(alpha)
 print(ALPHA ,"°")
 
 #distance
-T=int(6371000)
+T=int(6371009)
 phi1=l1*pi/180
 phi2=l2*pi/180
 Dphi1=A*pi/180
